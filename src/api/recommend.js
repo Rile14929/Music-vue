fef020f08faac34a61a17eb47de21bbea0e3b4ff
Liu <rile14929@163.com>
2018-08-02 00:02:07 +0800

    后端代理请求歌单列表

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -1,6 +1,11 @@
 import jsonp from 'common/js/jsonp'
-import {commonParams, options} from './config'
+import axios from 'axios'
+import {
+  commonParams,
+  options
+} from './config'
 
+// 获取轮播数据
 export function getRecommend () {
   const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
 
@@ -11,4 +16,26 @@ export function getRecommend () {
   })
 
   return jsonp(url, data, options)
+}
+
+// 获取歌单数据
+export function getDiscList () {
+  const url = '/api/getDiscList'
+
+  const data = Object.assign({}, commonParams, {
+    rnd: Math.random(),
+    hostUin: 0,
+    platform: 'yqq',
+    needNewCode: 0,
+    categoryId: 10000000,
+    sortId: 5,
+    sin: 0,
+    ein: 29,
+    format: 'json'
+  })
+  return axios.get(url, {
+    params: data
+  }).then((res) => {
+    return Promise.resolve(res.data)
+  })
 }
