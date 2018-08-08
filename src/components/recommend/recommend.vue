5d55531d465d313935599e8647857b4d7cf91c10
liuyuxuan <rile14929@163.com>
2018-08-08 15:42:56 +0800

    优化推荐页面

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,45 +1,49 @@
 <template>
-<div class="recommend" ref="recommend">
-  <scroll class="recommend-content" :data="discList">
-    <div v-if="recommends.length" class="slider-wrapper" ref="sliderWrapper">
-      <slider>
-        <div v-for="(item,index) in recommends" :key="index">
-          <a :href="item.linkUrl">
-            <img :src="item.picUrl" alt="">
-          </a>
+  <div class="recommend" ref="recommend">
+    <scroll ref="scroll" class="recommend-content" :data="discList">
+      <div>
+        <div v-if="recommends.length" class="slider-wrapper">
+          <div class="slider-content">
+            <slider ref="slider">
+              <div v-for="(item,index) in recommends" :key="index">
+                <a :href="item.linkUrl">
+                  <img @load="loadImage" :src="item.picUrl">
+                </a>
+              </div>
+            </slider>
+          </div>
         </div>
-      </slider>
-    </div>
-    <div class="recommend-list">
-      <h1 class="list-title">热门歌单推荐</h1>
-      <ul>
-        <li v-for="(item,index) in discList" class="item" :key="index">
-          <div class="icon">
-            <img width="60" height="60" :src="item.imgurl" alt="">
-          </div>
-          <div class="text">
-            <h2 class="name" v-html="item.creator.name"></h2>
-            <p class="desc" v-html="item.dissname"></p>
-          </div>
-        </li>
-      </ul>
-    </div>
-  </scroll>
-</div>
+        <div class="recommend-list">
+          <h1 class="list-title">热门歌单推荐</h1>
+          <ul>
+            <li @click="selectItem(item)" v-for="(item,index) in discList" class="item" :key="index">
+              <div class="icon">
+                <img width="60" height="60" v-lazy="item.imgurl">
+              </div>
+              <div class="text">
+                <h2 class="name" v-html="item.creator.name"></h2>
+                <p class="desc" v-html="item.dissname"></p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="loading-container" v-show="!discList.length">
+        <loading></loading>
+      </div>
+    </scroll>
+    <router-view></router-view>
+  </div>
 </template>
 
-<script>
+<script type="text/ecmascript-6">
 import Slider from 'base/slider/slider'
-import {getRecommend, getDiscList} from 'api/recommend'
-import {ERR_OK} from 'api/config'
+import Loading from 'base/loading/loading'
 import Scroll from 'base/scroll/scroll'
+import { getRecommend, getDiscList } from 'api/recommend'
+import { ERR_OK } from 'api/config'
 
 export default {
-  name: 'recommend',
-  components: {
-    Slider,
-    Scroll
-  },
   data () {
     return {
       recommends: [],
@@ -48,9 +52,35 @@ export default {
   },
   created () {
     this._getRecommend()
+
     this._getDiscList()
   },
+  activated () {
+    setTimeout(() => {
+      this.$refs.slider && this.$refs.slider.refresh()
+    }, 20)
+  },
   methods: {
+    handlePlaylist (playlist) {
+      const bottom = playlist.length > 0 ? '60px' : ''
+
+      this.$refs.recommend.style.bottom = bottom
+      this.$refs.scroll.refresh()
+    },
+    loadImage () {
+      if (!this.checkloaded) {
+        this.checkloaded = true
+        setTimeout(() => {
+          this.$refs.scroll.refresh()
+        }, 20)
+      }
+    },
+    selectItem (item) {
+      this.$router.push({
+        path: `/recommend/${item.dissid}`
+      })
+      this.setDisc(item)
+    },
     _getRecommend () {
       getRecommend().then((res) => {
         if (res.code === ERR_OK) {
@@ -65,51 +95,66 @@ export default {
         }
       })
     }
+  },
+  components: {
+    Slider,
+    Loading,
+    Scroll
   }
-
 }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
 @import '~common/stylus/variable';
 
-.recommend
+.recommend {
   position: fixed;
   width: 100%;
   top: 88px;
   bottom: 0;
-  .recommend-content
+
+  .recommend-content {
     height: 100%;
     overflow: hidden;
-    .slider-wrapper
+
+    .slider-wrapper {
       position: relative;
       width: 100%;
       height: 0;
       padding-top: 40%;
       overflow: hidden;
-      .slider-content
+
+      .slider-content {
         position: absolute;
         top: 0;
         left: 0;
         width: 100%;
         height: 100%;
-    .recommend-list
-      .list-title
+      }
+    }
+
+    .recommend-list {
+      .list-title {
         height: 65px;
         line-height: 65px;
         text-align: center;
         font-size: $font-size-medium;
         color: $color-theme;
-      .item
+      }
+
+      .item {
         display: flex;
         box-sizing: border-box;
         align-items: center;
         padding: 0 20px 20px 20px;
-        .icon
+
+        .icon {
           flex: 0 0 60px;
           width: 60px;
           padding-right: 20px;
-        .text
+        }
+
+        .text {
           display: flex;
           flex-direction: column;
           justify-content: center;
@@ -117,14 +162,25 @@ export default {
           line-height: 20px;
           overflow: hidden;
           font-size: $font-size-medium;
-          .name
+
+          .name {
             margin-bottom: 10px;
             color: $color-text;
-          .desc
+          }
+
+          .desc {
             color: $color-text-d;
-    .loading-container
+          }
+        }
+      }
+    }
+
+    .loading-container {
       position: absolute;
       width: 100%;
       top: 50%;
-      transform: translateY(-50%)
+      transform: translateY(-50%);
+    }
+  }
+}
 </style>
