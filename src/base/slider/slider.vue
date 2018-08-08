08677fda69aa02c3497e72a9d711ce0088e8fc8c
liuyuxuan <rile14929@163.com>
2018-08-08 14:35:18 +0800

    热门歌单推荐

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -5,23 +5,16 @@
       </slot>
     </div>
     <div class="dots">
-      <span class="dot" v-for="(item,index) in dots" :key="index" :class="{active:currentPageIndex === index}"></span>
+      <span class="dot" :class="{active: currentPageIndex === index }" v-for="(item, index) in dots" :key="index"></span>
     </div>
   </div>
 </template>
 
-<script>
-import BScroll from 'better-scroll'
+<script type="text/ecmascript-6">
 import { addClass } from 'common/js/dom'
-
+import BScroll from 'better-scroll'
 export default {
   name: 'slider',
-  data () {
-    return {
-      dots: [],
-      currentPageIndex: 0
-    }
-  },
   props: {
     loop: {
       type: Boolean,
@@ -36,13 +29,17 @@ export default {
       default: 4000
     }
   },
+  data () {
+    return {
+      dots: [],
+      currentPageIndex: 0
+    }
+  },
   mounted () {
     setTimeout(() => {
       this._setSliderWidth()
-      // 在初始化width之后初始化点
       this._initDots()
       this._initSlider()
-
       if (this.autoPlay) {
         this._play()
       }
@@ -64,7 +61,21 @@ export default {
       }, 60)
     })
   },
-  destroyed () {
+  activated () {
+    this.slider.enable()
+    let pageIndex = this.slider.getCurrentPage().pageX
+    this.slider.goToPage(pageIndex, 0, 0)
+    this.currentPageIndex = pageIndex
+    if (this.autoPlay) {
+      this._play()
+    }
+  },
+  deactivated () {
+    this.slider.disable()
+    clearTimeout(this.timer)
+  },
+  beforeDestroy () {
+    this.slider.disable()
     clearTimeout(this.timer)
   },
   methods: {
@@ -74,7 +85,6 @@ export default {
         this.slider.refresh()
       }
     },
-    // 设置宽度
     _setSliderWidth (isResize) {
       this.children = this.$refs.sliderGroup.children
       let width = 0
@@ -90,7 +100,6 @@ export default {
       }
       this.$refs.sliderGroup.style.width = width + 'px'
     },
-    // 初始化slider
     _initSlider () {
       this.slider = new BScroll(this.$refs.slider, {
         scrollX: true,
@@ -98,7 +107,7 @@ export default {
         momentum: false,
         snap: {
           loop: this.loop,
-          threshold: 0.3, // 可滚动到下一个的阈值
+          threshold: 0.3,
           speed: 400
         }
       })
@@ -114,10 +123,6 @@ export default {
         }
       })
     },
-    // 初始化下部的点
-    _initDots () {
-      this.dots = new Array(this.children.length)
-    },
     _onScrollEnd () {
       let pageIndex = this.slider.getCurrentPage().pageX
       this.currentPageIndex = pageIndex
@@ -125,13 +130,15 @@ export default {
         this._play()
       }
     },
+    _initDots () {
+      this.dots = new Array(this.children.length)
+    },
     _play () {
       clearTimeout(this.timer)
       this.timer = setTimeout(() => {
         this.slider.next()
       }, this.interval)
     }
-
   }
 }
 </script>
@@ -141,7 +148,6 @@ export default {
 
 .slider {
   min-height: 1px;
-  position: relative;
 
   .slider-group {
     position: relative;
@@ -173,6 +179,7 @@ export default {
     right: 0;
     left: 0;
     bottom: 12px;
+    transform: translateZ(1px);
     text-align: center;
     font-size: 0;
 
